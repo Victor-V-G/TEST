0d1c43ac61f4754a4352bf1ebef90b3a6ba7f23e
Victor-V-G <victorvicencio932@gmail.com>
2025-05-05 14:35:22 -0400

    JS FORMULARIOS SIMPLIFICADO
    AQUI UNA MEJOR MANERA Y MAS SIMPLIFICADA DE ABORDAR VALIDACIONES Y FUNCIONES

--- assets/js/scriptindex.js
+++ assets/js/scriptindex.js
@@ -4,46 +4,37 @@ en el boton, se ejecutara este script*/
 function validar(){
     /*SEGMENTO DE LA CELDA NOMBRE*/
     let eNombre = document.getElementById("nombre") /*RECUPERA EL ELEMENTO MEDIANTE LA ID NOMBRE, ALMACENANDOLO EN eNombre */
-    let vNombre = eNombre.value.trim()
+    let vNombre = eNombre.value
     let eErrorNombre = document.getElementById("errorNombre")
-    if(vNombre.length==0 ){
-        console.log("Debes completar este campo") /*SIRVE PARA MOSTRAR UN MENSAJE EN LA CONSOLA (NAVEGADOR) */
-        alert("Debes completar este campo") /*SIRVE PARA MOSTRAR UNA ALERTA (VENTANA EMERGENTE) */
-    }
-    else if(vNombre.length > 3){
-        console.log("Cumple") /*SIRVE PARA MOSTRAR UN MENSAJE EN LA CONSOLA (NAVEGADOR) */
-        eErrorNombre.innerText = "" /*SIRVE PARA OCULTAR EL MENSAJE DE ERROR*/
-        eNombre.style.backgroundColor = "green" /*ESTABLECE EL COLOR DE FONDO DE LA CELDA COMO VERDE SI ESTA PASA LA VALIDACION */
-        eNombre.style.color = "white" /*ESTABLECE EL COLOR DE LA LETRA DENTRO DE LA CELDA DE COLOR BLANCO */
-    }
-    else{
-        console.log("Error, pocos caracteres.") /*SIRVE PARA MOSTRAR UN MENSAJE EN LA CONSOLA (NAVEGADOR) */
-        alert("Debes ingresar al menos 3 caracteres") /*SIRVE PARA MOSTRAR UNA ALERTA (VENTANA EMERGENTE) */
-        eErrorNombre.innerText = "Debes ingresar 3 caracteres como minimo" /*SIRVE PARA MOSTRAR UN MENSAJE DE ERROR*/
-        eNombre.style.backgroundColor = "red" /*ESTABLECE EL COLOR DE FONDO DE LA CELDA COMO ROJO SI ESTA NO PASA LA VALIDACION */
-        eNombre.style.color = "white" /*ESTABLECE EL COLOR DE LA LETRA DENTRO DE LA CELDA DE COLOR BLANCO */
-    }
-
-    /*SEGMENTO DE LA CELDA APELLIDO*/
+    
     let eApellido = document.getElementById("apellido")
     let vApellido = eApellido.value
     let eErrorApellido = document.getElementById("errorApellido")
-    if(vApellido.length==0 ){
+
+    /*SIRVE PARA LLAMAR A LA FUNCION VALIDARLARGOMINIMO */
+    validarLargoMinimo(eNombre,vNombre,eErrorNombre)
+    validarLargoMinimo(eApellido,vApellido,eErrorApellido)
+   
+
+}
+
+/*OTRA MANERA PARA SIMPLIFICAR LAS FUNCIONES Y VALIDACIONES */
+function validarLargoMinimo(elemento,valor,eError){
+    if(valor.length==0 ){
         console.log("Debes completar este campo")
         alert("Debes completar este campo")
     }
-    else if(vApellido.length > 3){
+    else if(valor.length > 3){
         console.log("Cumple")
-        eErrorApellido.innerText = ""
-        eApellido.style.backgroundColor = "green"
-        eApellido.style.color = "white"
+        eError.innerText = ""
+        elemento.style.backgroundColor = "green"
+        elemento.style.color = "white"
     }
     else{
         console.log("Error, Pocos caracteres")
         alert("Debes ingresar al menos 3 caracteres")
-        eErrorApellido.innerText = "Debes ingresar 3 caracteres como minimo"
-        eApellido.style.backgroundColor = "red"
-        eApellido.style.color = "white"
+        eError.innerText = "Debes ingresar 3 caracteres como minimo"
+        elemento.style.backgroundColor = "red"
+        elemento.style.color = "white"
     }
-
 }
